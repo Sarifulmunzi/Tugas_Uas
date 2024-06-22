d8bf2eedf20e293a427593c8c9778f8b39e3a1ca
Sarifulmunzi <133507570+Sarifulmunzi@users.noreply.github.com>
2024-06-22 09:13:53 +0700

    Add files via upload

---
+++ book store/book-store/src/main/java/com/example/book/store/MunziApplication.java
@@ -0,0 +1,13 @@
+package com.example.book.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RifkiSarifulMunziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RifkiSarifulMunziApplication.class, args);
+	}
+
+}
